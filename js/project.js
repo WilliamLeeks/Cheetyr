bb47eb774924f06de556fd51ee73142035a4775c
Will Leeks <will@williamleeks.com>
2014-08-04 12:27:32 +0100

    Add live filter js

--- js/project.js
+++ js/project.js
@@ -16,3 +16,23 @@ if (!Modernizr.svg) {
   $(".brand").attr("src", "img/cheetyr-logo.png");
   $(".wl-brand").attr("src", "img/william-leeks-logo.png");
 }
+
+// Initiate live filter
+$(function() {
+    $('#search-sheet').fastLiveFilter('.command-list');
+});
+
+// Check if lists are hidden
+function checkEmptyLists() {
+	$(".command-list").filter(function() {
+		return ($(this).children(":visible").length == 0)
+	}).addClass('no-border').prev().hide();
+}
+myVar = setInterval(checkEmptyLists, 100);
+
+function checkLists() {
+	$(".command-list").filter(function() {
+		return ($(this).children(":visible").length > 0)
+	}).removeClass('no-border').prev().show();
+}
+myVar = setInterval(checkLists, 100);
