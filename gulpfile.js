e087c1d96ba871a2032f2dfcae2d453d4d03e2e7
Will Leeks <will@williamleeks.com>
2014-09-17 12:10:21 +0100

    Add rename and uglify to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var autoprefix = require('gulp-autoprefixer');
 var sass = require('gulp-ruby-sass');
 var notify = require('gulp-notify');
 var livereload = require('gulp-livereload');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('css', function() {
 	return gulp.src('scss/style.scss')
@@ -14,9 +16,18 @@ gulp.task('css', function() {
 		.pipe(notify({message: project + " CSS Updated."}));
 });
 
+gulp.task('js', function() {
+	gulp.src('js/project.js')
+		.pipe(uglify())
+		.pipe(rename('project.min.js'))
+		.pipe(gulp.dest('js'))
+		.pipe(notify({message: project + " project JS minified."}));
+});
+
 gulp.task('watch', function() {
 	livereload.listen();
 	gulp.watch('scss/*.scss', ['css']);
+	gulp.watch('js/project.js', ['js']);
 	gulp.watch(['css/*.css', '*.php']).on('change', livereload.changed);
 });
 
