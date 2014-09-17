105c9b4a01799bf2babb30cfaff9051930bf8685
Will Leeks <mail@williamleeks.com>
2014-09-17 16:45:53 +0100

    Add replace text script

--- footer.php
+++ footer.php
@@ -5,6 +5,10 @@
 <!-- Search Filter -->
 <script src="../js/jquery.fastLiveFilter.js"></script>
 
+<!-- Find and replace Plugin -->
+<script src="../js/jquery.ba-replacetext.min.js"></script>
+
+<!-- Project jQuery -->
 <script src="../js/project.min.js"></script>
 
 </html>
