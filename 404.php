c9b571f2b94e699a41495fb751ca1d0e40cd60da
Will Leeks <mail@williamleeks.com>
2014-09-17 15:22:04 +0100

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+	$title = "Cheetyr – Cheatsheets for Designers &amp; Developers";
+	include 'header.php';
+?>
+	<main>
+		<h1>Page Not Found</h1>
+		<p class="lede">Sorry, that page doesn't seem to exist. Perhaps you were after one of the following pages?</p>
+		<?php include 'nav.php'; ?>
+	</main>
+<?php include 'footer.php'; ?>
