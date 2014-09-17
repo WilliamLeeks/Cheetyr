c3732dfcc1a6c82962b2c9d1c95f84a141072f4b
Will Leeks <mail@williamleeks.com>
2014-09-17 15:26:49 +0100

    Set up routing

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php
+	$title = "Cheetyr – Cheatsheets for Designers &amp; Developers";
+	include 'header.php';
+?>
+	<main>
+		<h1>Welcome to Cheetyr</h1>
+		<p class="lede">A handy collection of cheatsheets and shortcuts to speed up the work of designers and developers.</p>
+		<p>This project is very much a work in progress, and I hope in time to make it as comprehensive as possible. If you are interested in helping it grow, or just have a little feedback, please <a href="http://williamleeks.com/contact.php">get in touch</a> or get involved on <a href="https://github.com/WilliamLeeks/Cheetyr">GitHub</a>.</p>
+	</main>
+<?php include 'footer.php'; ?>
