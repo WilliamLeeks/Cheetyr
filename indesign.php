1f30e61c0db21875e6c2736de54910f06a002d2e
Will Leeks <mail@williamleeks.com>
2014-09-25 11:12:25 +0100

    Add indesign page and data

---
+++ indesign.php
@@ -0,0 +1,8 @@
+<?php
+	$sheet = 'indesign';
+	$pageTitle = 'InDesign';
+	$title = "Cheetyr – Adobe $pageTitle Cheatsheet";
+	include 'header.php';
+?>
+<?php include 'main.php'; ?>
+<?php include 'footer.php'; ?>
