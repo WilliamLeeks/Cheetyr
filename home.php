f9989aea11474fefc759721ad2ef4aa7531e804b
Will Leeks <mail@williamleeks.com>
2015-02-17 15:05:41 +0000

    Update homepage copy

--- home.php
+++ home.php
@@ -1,10 +1,12 @@
 <?php
 	$title = "Cheetyr – Cheatsheets for Designers &amp; Developers";
+	$pageTitle = "home";
 	include 'header.php';
 ?>
 	<main>
 		<h1>Welcome to Cheetyr</h1>
 		<p class="lede">A handy collection of cheatsheets and shortcuts to speed up the work of designers and developers.</p>
 		<p>This project is very much a work in progress, and I hope in time to make it as comprehensive as possible. If you are interested in helping it grow, or just have a little feedback, please <a href="http://williamleeks.com/contact.php">get in touch</a> or get involved on <a href="https://github.com/WilliamLeeks/Cheetyr">GitHub</a>.</p>
+		<p>At present, where shortcuts vary by operating system, the shortcuts on this site are for the Mac version of the software in question.</p>
 	</main>
 <?php include 'footer.php'; ?>
